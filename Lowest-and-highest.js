b4e3b961983a6d5f89f1bb02e4e758e15b6d3566
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-09-29 23:39:32 +0200

    Update Lowest-and-highest.js

--- Lowest-and-highest.js
+++ Lowest-and-highest.js
@@ -23,3 +23,16 @@ function highAndLow(numbers){
 }
 
 // or
+
+function highAndLow(numbers){
+  let numArray = numbers.split(" ");
+  let max      = Number(numArray[0]);
+  let min      = Number(numArray[0]);
+  let num;
+  for(let i = 1; i< numArray.length; i++){
+    num = Number(numArray[i]);
+    if(num > max) max = num;
+    if(num < min) min = num;
+  }
+  return max + " " + min;
+  }
