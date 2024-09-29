86aa8b123ad318be247a41aeeda553bbf7dcbda5
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-09-29 23:35:55 +0200

    Update Lowest-and-highest.js

--- Lowest-and-highest.js
+++ Lowest-and-highest.js
@@ -13,3 +13,6 @@ Notes:
   There will always be at least one number in the input string.
   Output string must be two numbers separated by a single space, and highest number is first.
 */
+
+
+// Solution
